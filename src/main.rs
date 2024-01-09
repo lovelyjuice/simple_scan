58d352ed3daa0d23c77189a4aab11178769a20c6
John Doe <johndoe@example.com>
2024-01-09 19:04:58 +0800

    linux下无管理员权限则使用ping命令

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,19 @@ use std::process::exit;
 use std::str::FromStr;
 use std::sync::{Arc, mpsc};
 
+#[cfg(target_os = "linux")]
+use std::{sync::OnceLock};
+
+#[cfg(target_os = "linux")]
+static USE_PING_CMD: OnceLock<bool> = OnceLock::new();
+
 use clap;
 use clap::{Arg, Command, value_parser};
 use ipnet::{Ipv4AddrRange, Ipv4Net};
 use surge_ping;
 use tokio;
 use tokio::net::TcpSocket;
-use tokio::{io, time};
+use tokio::{time};
 use tokio::time::Duration;
 
 fn main() {
@@ -102,7 +108,7 @@ async fn start() {
     let gateway_discovery = matches.get_flag("no_gateway_discovery");
     let ping_discovery = matches.get_flag("no_ping_discovery");
     let port_discovery = matches.get_flag("no_port_discovery");
-    let discovery_ports: Vec<u16> = parse_ports(matches.get_one::<String>("discovery_ports").unwrap().clone());
+    let mut discovery_ports: Vec<u16> = parse_ports(matches.get_one::<String>("discovery_ports").unwrap().clone());
     let wait_time: u8 = matches.get_one::<u8>("wait_time").unwrap().to_owned();
     let connect_config = ConnectConfig { timeout, retry, wait_time };
     let infile = if let Some(infile) = matches.get_one::<String>("infile") {
@@ -121,6 +127,19 @@ async fn start() {
         parse_hosts(&content)
     } else { parse_hosts(scan_targets.unwrap()) };
     let mut need_scan_host = Arc::new(net_hosts);    // 当扫描10.0.0.0/8时，net_hosts占用60M空间，由于其他线程需要使用该变量，因此使用Arc可以降低扫描时一半的内存占用
+
+    #[cfg(target_os = "linux")]{
+        if ping_discovery {
+            let payload = [0; 8];
+            match surge_ping::ping("127.0.0.1".parse().unwrap(), &payload).await {
+                Ok(_) => { USE_PING_CMD.set(false).unwrap(); }
+                Err(e) => {
+                    println!("Can not use raw socket to ping, because {}.\nTry to use ping command", e.to_string());
+                    USE_PING_CMD.set(true).unwrap();
+                }
+            }
+        }
+    }
 
     // 网关存活探测
     let mut alive_subnets = vec![];
@@ -177,6 +196,7 @@ async fn start() {
     if port_discovery {
         println!("Scanning common ports to discover alive host.");
         let (tx, rx) = mpsc::channel();
+        discovery_ports.retain(|&port| ports.contains(&port));   // 用户不想扫的端口不作为探测用的端口
         tokio::spawn(
             port_scan_with_channel(need_scan_host.clone(), discovery_ports.clone(), concurrency.clone(), connect_config.clone(), tx)
         );
@@ -184,7 +204,7 @@ async fn start() {
             println!("Port discovery {}:{} is alive", host_port.host, host_port.port);
             alive_hosts.insert(host_port.host);
             if let Some(ref mut outfile) = outfile {
-                writeln!(outfile, "{}:{}", host_port.host, host_port.port);
+                write!(outfile, "{}:{}\r\n", host_port.host, host_port.port);
             }
             discovery_services.entry(host_port.host).or_insert_with(Vec::new).push(host_port.port);
         }
@@ -221,7 +241,7 @@ async fn start() {
         ports_num += 1;
         println!("{}:{}", host_port.host, host_port.port);
         if let Some(ref mut outfile) = outfile {
-            writeln!(outfile, "{}:{}", host_port.host, host_port.port);
+            write!(outfile, "{}:{}\r\n", host_port.host, host_port.port);
         }
     }
     if let Some(mut outfile) = outfile {
@@ -252,22 +272,66 @@ async fn ping_scan_with_channel(
             let _permit = permit;
             let payload = [0; 8];
             for _ in 0..=retry {
-                match tokio::time::timeout(
-                    Duration::from_secs(timeout as u64),
-                    surge_ping::ping(IpAddr::V4(host.clone()), &payload),
-                ).await
-                {
-                    Ok(timeout_result) => {
-                        if let Ok(_ping_result) = timeout_result {
+                #[cfg(target_os = "linux")]{
+                    if *USE_PING_CMD.get().unwrap() {
+                        if cmd_ping(&host, &timeout).await {
                             tx.send(host);
                             break;
+                        };
+                    } else {
+                        match tokio::time::timeout(
+                            Duration::from_secs(timeout as u64),
+                            surge_ping::ping(IpAddr::V4(host.clone()), &payload),
+                        ).await
+                        {
+                            Ok(timeout_result) => {
+                                match timeout_result {
+                                    Ok(_ping_result) => {
+                                        tx.send(host);
+                                        break;
+                                    }
+                                    Err(e) => {
+                                        eprintln!("ping error: {}", e.to_string());
+                                    }
+                                }
+                            }
+                            Err(_) => {} // 超时
                         }
                     }
-                    Err(_) => {} // 超时
+                }
+                #[cfg(target_os = "windows")]{
+                    match tokio::time::timeout(
+                        Duration::from_secs(timeout as u64),
+                        surge_ping::ping(IpAddr::V4(host.clone()), &payload),
+                    ).await
+                    {
+                        Ok(timeout_result) => {
+                            match timeout_result {
+                                Ok(_ping_result) => {
+                                    tx.send(host);
+                                    break;
+                                }
+                                Err(e) => {
+                                    eprintln!("ping error: {}", e.to_string());
+                                }
+                            }
+                        }
+                        Err(_) => {} // 超时
+                    }
                 }
             }
         });
     }
+}
+
+#[cfg(target_os = "linux")]
+async fn cmd_ping(ip: &Ipv4Addr, timeout: &u8) -> bool {
+    let output = tokio::process::Command::new("ping").args(["-c", "1", "-W", &timeout.to_string(), &ip.to_string()]).output().await.unwrap();
+    let raw_output = String::from_utf8(output.stdout).unwrap();
+    if raw_output.contains("100% packet loss") {
+        return false;
+    }
+    return true;
 }
 
 async fn port_scan_with_channel(
@@ -314,7 +378,7 @@ async fn connect(ip: Ipv4Addr, port: u16, config: &ConnectConfig) -> bool {
                         stream.writable().await;
                         let mut buffer = [0; 1];
                         match stream.try_write(&mut buffer) {
-                            Ok(n) => {
+                            Ok(_n) => {
                                 return true;    // 写数据成功说明tcp连接正常
                             }
                             Err(_) => {
@@ -326,7 +390,10 @@ async fn connect(ip: Ipv4Addr, port: u16, config: &ConnectConfig) -> bool {
                 }
                 return false; // 没超时但连接异常
             }
-            Err(_) => { // 超时
+            Err(e) => { // 超时
+                let mut error_string = e.to_string();
+                assert!(!error_string.to_lowercase().contains("too many open files"), "Too many open files. Please reduce batch size. The default is 5000. Try -b 2500.");
+
                 // println!("{}:{}超时", ip, port);
             }
         }
@@ -360,13 +427,15 @@ fn parse_ports(ports: String) -> Vec<u16> {
         ("goby_default", "20,21,22,23,25,26,30,31,32,36,37,38,43,49,51,53,67,70,79,80,81,82,83,84,85,86,87,88,89,98,102,104,110,111,113,119,121,135,138,139,143,175,179,199,211,264,280,311,389,443,444,445,449,465,500,502,503,505,512,515,540,548,554,564,587,620,631,636,646,666,771,777,789,800,801,808,873,876,880,888,898,900,901,902,990,992,993,994,995,999,1000,1010,1022,1023,1024,1025,1026,1027,1042,1080,1099,1177,1194,1200,1201,1212,1214,1234,1241,1248,1260,1290,1302,1311,1314,1344,1389,1400,1433,1443,1471,1494,1503,1505,1515,1521,1554,1588,1610,1720,1723,1741,1777,1830,1863,1880,1883,1901,1911,1935,1947,1962,1967,1991,2000,2001,2002,2010,2020,2022,2030,2049,2051,2052,2053,2055,2064,2077,2080,2082,2083,2086,2087,2095,2096,2121,2154,2160,2181,2222,2223,2252,2306,2323,2332,2375,2376,2379,2396,2401,2404,2406,2424,2443,2455,2480,2501,2525,2600,2601,2604,2628,2715,2809,2869,3000,3001,3002,3005,3050,3052,3075,3097,3128,3260,3280,3288,3299,3306,3307,3310,3311,3312,3333,3337,3352,3372,3388,3389,3390,3443,3460,3520,3522,3523,3524,3525,3528,3531,3541,3542,3567,3689,3690,3749,3780,3790,4000,4022,4040,4063,4064,4155,4200,4300,4369,4430,4433,4440,4443,4444,4505,4506,4567,4660,4664,4711,4712,4730,4782,4786,4840,4842,4848,4880,4911,4949,5000,5001,5002,5003,5004,5005,5006,5007,5008,5009,5010,5038,5050,5051,5060,5061,5080,5084,5111,5222,5258,5269,5280,5357,5400,5427,5432,5443,5550,5555,5560,5577,5598,5631,5672,5678,5800,5801,5802,5820,5900,5901,5902,5903,5938,5984,5985,5986,6000,6001,6002,6003,6004,6005,6006,6007,6008,6009,6010,6060,6068,6080,6082,6103,6346,6363,6379,6443,6488,6544,6560,6565,6581,6588,6590,6600,6664,6665,6666,6667,6668,6669,6697,6699,6780,6782,6868,6998,7000,7001,7002,7003,7004,7005,7007,7010,7014,7070,7071,7077,7080,7100,7144,7145,7170,7171,7180,7187,7199,7272,7288,7382,7401,7402,7443,7474,7479,7493,7500,7537,7547,7548,7634,7657,7676,7776,7777,7778,7779,7780,7788,7911,8000,8001,8002,8003,8004,8005,8006,8007,8008,8009,8010,8020,8025,8030,8032,8040,8058,8060,8069,8080,8081,8082,8083,8084,8085,8086,8087,8088,8089,8090,8091,8092,8093,8094,8095,8096,8097,8098,8099,8111,8112,8118,8123,8125,8126,8129,8138,8139,8140,8159,8161,8181,8182,8194,8200,8222,8291,8332,8333,8334,8351,8377,8378,8388,8443,8444,8480,8500,8529,8545,8546,8554,8567,8649,8686,8688,8765,8800,8834,8880,8881,8882,8883,8884,8885,8886,8887,8888,8889,8890,8899,8983,8999,9000,9001,9002,9003,9004,9005,9006,9007,9008,9009,9010,9030,9042,9050,9051,9080,9083,9090,9091,9100,9151,9191,9200,9229,9292,9295,9300,9306,9333,9334,9418,9443,9444,9446,9527,9530,9595,9653,9668,9700,9711,9801,9864,9869,9870,9876,9943,9944,9981,9997,9999,10000,10001,10003,10005,10030,10035,10162,10243,10250,10255,10332,10333,10389,10443,10554,10909,10911,10912,11001,11211,11300,11310,11371,11965,12000,12300,12345,12999,13579,13666,13720,13722,14000,14147,14265,14443,14534,15000,16000,16010,16030,16922,16923,16992,16993,17000,17988,18000,18001,18080,18081,18086,18245,18246,18264,19150,19888,19999,20000,20005,20332,20547,20880,22105,22222,22335,23023,23424,25000,25010,25105,25565,26214,26470,27015,27016,27017,28017,28080,29876,29999,30001,30005,31337,32400,32770,32771,32773,33338,33890,34567,34599,37215,37777,40000,40001,41795,42873,44158,44818,45554,49151,49152,49153,49154,49155,49156,49157,49158,49159,49664,49665,49666,49667,49668,49669,49670,49671,49672,49673,49674,50000,50050,50060,50070,50075,50090,50100,50111,51106,52869,55442,55553,55555,60001,60010,60030,60443,61613,61616,62078,64738"),
         ("goby_common", "1,7,9,13,19,21-23,25,37,42,49,53,69,79-81,85,105,109-111,113,123,135,137-139,143,161,179,222,264,384,389,402,407,443-446,465,500,502,512-515,523-524,540,548,554,587,617,623,689,705,771,783,873,888,902,910,912,921,993,995,998,1000,1024,1030,1035,1090,1098-1103,1128-1129,1158,1199,1211,1220,1234,1241,1300,1311,1352,1433-1435,1440,1494,1521,1530,1533,1581-1582,1604,1720,1723,1755,1811,1900,2000-2001,2049,2082,2083,2100,2103,2121,2199,2207,2222,2323,2362,2375,2380-2381,2525,2533,2598,2601,2604,2638,2809,2947,2967,3000,3037,3050,3057,3128,3200,3217,3273,3299,3306,3311,3312,3389,3460,3500,3628,3632,3690,3780,3790,3817,4000,4322,4433,4444-4445,4659,4679,4848,5000,5038,5040,5051,5060-5061,5093,5168,5247,5250,5351,5353,5355,5400,5405,5432-5433,5498,5520-5521,5554-5555,5560,5580,5601,5631-5632,5666,5800,5814,5900-5910,5920,5984-5986,6000,6050,6060,6070,6080,6082,6101,6106,6112,6262,6379,6405,6502-6504,6542,6660-6661,6667,6905,6988,7001,7021,7071,7080,7144,7181,7210,7443,7510,7579-7580,7700,7770,7777-7778,7787,7800-7801,7879,7902,8000-8001,8008,8014,8020,8023,8028,8030,8080-8082,8087,8090,8095,8161,8180,8205,8222,8300,8303,8333,8400,8443-8444,8503,8800,8812,8834,8880,8888-8890,8899,8901-8903,9000,9002,9060,9080-9081,9084,9090,9099-9100,9111,9152,9200,9390-9391,9443,9495,9809-9815,9855,9999-10001,10008,10050-10051,10080,10098,10162,10202-10203,10443,10616,10628,11000,11099,11211,11234,11333,12174,12203,12221,12345,12397,12401,13364,13500,13838,14330,15200,16102,17185,17200,18881,19300,19810,20010,20031,20034,20101,20111,20171,20222,22222,23472,23791,23943,25000,25025,26000,26122,27000,27017,27888,28222,28784,30000,30718,31001,31099,32764,32913,34205,34443,37718,38080,38292,40007,41025,41080,41523-41524,44334,44818,45230,46823-46824,47001-47002,48899,49152,50000-50004,50013,50500-50504,52302,55553,57772,62078,62514,65535"),
         ("db", "1433,1521,3306,5000,5236,5432,6379,9002,11211,27017"),
-        ("web", "80-86,443,2443,3443,7443,8443,8080-8086,8009")
+        ("web", "80-86,443,2443,3443,7443,8443,8080-8086,8009"),
+        ("win", "135,139,445,3389,5985,5986,47001,49152,57772,57773,57774,57775,57776,57777,57778"),
+        ("web_100", "80,81,82,83,84,85,86,88,89,90,91,99,443,800,801,808,880,888,889,1000,1010,1980,2018,2019,3000,3505,5555,6080,6677,7000,7001,7002,7003,7005,7007,7070,7080,7200,7777,7890,8000,8001,8002,8003,8004,8006,8008,8009,8010,8011,8012,8014,8016,8020,8028,8042,8053,8060,8069,8070,8080,8081,8082,8083,8084,8085,8086,8087,8088,8089,8090,8091,8099,8100,8118,8123,8161,8172,8180,8181,8200,8222,8243,8251,8280,8281,8300,8333,8360,8443,8484,8500,8800,8834,8879,8880,8881,8888,8899,8983,8989,9000,9001,9002,9043,9060,9080,9081,9085,9090,9091,9200,9443,9800,9981,9999,10000,10001,10002,10003,12443,15672,16080,18080,18082,18091,18092,20720,28017,38501,38888"),   // https://github.com/cwkiller/Pentest_Dic/blob/master/端口/乌云漏洞库出现漏洞的端口Top100.txt
     ]);
     let ports = ports.split(',')
-        .map(|port| ports_map.get(port).unwrap_or(&port).clone())
+        .map(|port| ports_map.get(port).unwrap_or(&port).to_owned())
         .collect::<Vec<&str>>().join(",");
     let mut port_list = ports_to_vec(&ports);
-    if port_list.len() > 10000 {    // 超过一万则先扫常见端口
+    if port_list.len() > 1000 {    // 超过1000则先扫常见端口
         let common_ports = ports_to_vec(ports_map.get("goby_common").unwrap());
         port_list.sort_by_cached_key(|port| common_ports.iter().position(|x| x == port).unwrap_or(usize::MAX));
     }
